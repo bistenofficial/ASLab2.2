d27560fb6c9e5e5f787038899f94216791397457
bistenofficial <maks.kirov.2002@mail.ru>
2021-04-09 14:25:46 +0300

    Start work

--- app/src/main/java/com/example/aslab22/MainActivity.java
+++ app/src/main/java/com/example/aslab22/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.aslab22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+public EditText Height;
+    public EditText Weight;
+    public EditText Age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Height = (EditText)findViewById(R.id.editTextNumberHeight);
+        Weight = (EditText)findViewById(R.id.editTextNumberWeight);
+        Age = (EditText)findViewById(R.id.editTextNumberAge);
+    }
+    public void cls(View v)
+    {
+        Height.setText("");
+        Weight.setText("");
+        Age.setText("");
     }
 }
